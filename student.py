cf8ee573c6a471bdc7fbd9fef719e35ed6fa0391
jaydeepgaikwad <47100161+jaydeepgaikwad@users.noreply.github.com>
2019-01-28 13:58:43 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,12 @@
+#name:gaikwad jaydeep
+#div:p       #batch:1
+
+
+d={}
+x=int(input("enter a number"))
+for i in range(x):
+ n=input("name")
+ g=int(input("gr.no"))
+ b=input("branch")
+ d[g]=(n,b)
+ print(d)
